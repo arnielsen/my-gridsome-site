f094b88f4a7629bab48cbc297e5d8574ce855255
Anders Røndbjerg Nielsen <arnielsen@outlook.dk>
2019-10-12 14:50:33 +0200

    first commit

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <div class="flex text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight">
+                <g-link to="/">
+                    {{$static.metadata.siteName}}
+                </g-link>
+            </span>
+        </div>
+    </header>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+    name: "HeaderBar",
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    <NavBar />
+    <slot/>
+    
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+
+<style>
+
+</style>
